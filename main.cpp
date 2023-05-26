e41f2795f6694087711a13a9363e22fca4005ce5
KonfetA <KonfetA@gmail.com>
2023-05-26 04:02:24 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,33 +1,88 @@
-#include <array>
 #include <SFML/Graphics.hpp>
 #include <time.h>
 
-using namespace sf;
-using namespace std;
-
-struct Tile {
-	bool open = false;
-	bool mine = false;
-	bool flag = false;
-	int count = 0;
-};
-
 const int gridWidth = 10, gridHight = 10, imageWidth = 32;
-Tile grid[gridWidth][gridHight];
+const int spritesAmount = 12;
+
+sf::Texture texture;
+sf::Font font;
+sf::Sprite sprites[spritesAmount];
+sf::Clock Clock;
+
+int seconds = 0;
 bool firstPress = true;
 
+class Tile {
+private:
+public:
+	enum class TileState {
+		open, flaged, closed
+	};
+
+	TileState tileState = Tile::TileState::closed;
+	bool isMine = false;
+	int count = 0;
+
+	sf::Sprite getSprite() {
+		switch (tileState) {
+			case Tile::TileState::open:
+				return isMine ? sprites[9] : sprites[count];
+				break;
+			case Tile::TileState::flaged:
+				return sprites[11];
+				break;
+			case Tile::TileState::closed:
+				return sprites[10];
+				break;
+		}
+	}
+
+	void toggleFlag() {
+		switch (tileState) {
+			case Tile::TileState::flaged:
+				tileState = TileState::closed;
+				break;
+			case Tile::TileState::closed:
+				tileState = TileState::flaged;
+				break;
+		}
+	}
+} grid[gridWidth][gridHight];
+
+void setup() {
+	srand(time(0));
+
+	Clock.restart();
+
+	texture.loadFromFile("images/Tiles.png");
+	font.loadFromFile("CyrilicOld.TTF");
+
+	for (size_t i = 0; i < spritesAmount; i++) {
+		sprites[i].setTexture(texture);
+		sprites[i].setTextureRect(sf::IntRect(i * imageWidth, 0, imageWidth, imageWidth));
+	}
+}
+
+bool areCoordsValid(int x, int y) {
+	return
+		x >= 0 && x < gridWidth &&
+		y >= 0 && y < gridHight;
+}
+
 void openTile(int x, int y) {
+	grid[x][y].tileState = Tile::TileState::open;
+
+	if (grid[x][y].count != 0)
+		return;
+
 	for (int dx : {-1, 0, 1}) {
 		for (int dy : {-1, 0, 1}) {
 			int x2 = x + dx, y2 = y + dy;
 
-			if (!(x2 >= 0 && x2 < gridWidth && y2 >= 0 && y2 < gridHight))
+			if (!areCoordsValid(x2, y2))
 				continue;
 
-			grid[x][y].open = true;
-
-			if (grid[x][y].count == 0 && grid[x2][y2].mine == false &&
-				grid[x2][y2].open == false && grid[x2][y2].flag == false) {
+			if (grid[x2][y2].tileState == Tile::TileState::closed) {
 				openTile(x2, y2);;
 			}
 		}
@@ -38,18 +93,18 @@ void generateBomb(int x, int y) {
 	for (int i = 0; i < gridWidth * gridHight / 5; i++) {
 		int xB = rand() % gridWidth, yB = rand() % gridHight;
 
-		while (xB == x && yB == y || grid[xB][yB].mine == true) {
+		while (xB == x && yB == y || grid[xB][yB].isMine == true) {
 			xB = rand() % gridWidth;
 			yB = rand() % gridHight;
 		}
 
-		grid[xB][yB].mine = true;
+		grid[xB][yB].isMine = true;
 
 		for (int dx : {-1, 0, 1}) {
 			for (int dy : {-1, 0, 1}) {
 				int x2 = xB + dx, y2 = yB + dy;
 
-				if (x2 >= 0 && x2 < gridWidth && y2 >= 0 && y2 < gridHight) {
+				if (areCoordsValid(x2, y2)) {
 					grid[x2][y2].count++;
 				}
 			}
@@ -57,76 +112,72 @@ void generateBomb(int x, int y) {
 	}
 }
 
-void gridEvent(RenderWindow& window) {
-	Event event;
+void Megumin() {
+	sf::RenderWindow megumin(sf::VideoMode(1644, 2089), "Megumin!");
+	sf::Texture texture;
+	texture.loadFromFile("images/BAH.png");
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
+	while (megumin.isOpen()) {
+		sf::Event event;
+
+		while (megumin.pollEvent(event))
+			if (event.type == sf::Event::Closed)
+				megumin.close();
+
+		megumin.draw(sprite);
+		megumin.display();
+	}
+}
+
+void gridEvent(sf::RenderWindow& window) {
+	sf::Event event;
 
 	while (window.pollEvent(event)) {
-		if (event.type == Event::Closed)
+		if (event.type == sf::Event::Closed)
 			window.close();
 
-		if (event.type == Event::MouseButtonPressed) {
-			Vector2i pos = Mouse::getPosition(window);
+		if (event.type == sf::Event::MouseButtonPressed) {
+			sf::Vector2i pos = sf::Mouse::getPosition(window);
 			int x = pos.x / 32;
 			int y = pos.y / 32;
 
-			if (x >= 0 && x < gridWidth && y >= 0 && y < gridHight) {
-				if (event.key.code == Mouse::Right && grid[x][y].open == false)
-					grid[x][y].flag = !grid[x][y].flag;
+			if (!areCoordsValid(x, y) || grid[x][y].tileState == Tile::TileState::open)
+				return;
 
-				if (event.key.code == Mouse::Left && grid[x][y].flag == false) {
-					if (firstPress) {
-						generateBomb(x, y);
-						firstPress = false;
-					}
+			if (event.key.code == sf::Mouse::Right)
+				grid[x][y].toggleFlag();
 
-					openTile(x, y);
+			if (event.key.code == sf::Mouse::Left && grid[x][y].tileState != Tile::TileState::flaged) {
+				if (firstPress) {
+					generateBomb(x, y);
+					firstPress = false;
+				}
 
-					if (grid[x][y].mine == true) {
-						RenderWindow Megumin(VideoMode(1644, 2089), "Megumin!");
-						Texture texture;
-						texture.loadFromFile("images/BAH.png");
-						Sprite sprite;
-						sprite.setTexture(texture);
+				openTile(x, y);
 
-						while (Megumin.isOpen()) {
-							Event event;
-
-							while (Megumin.pollEvent(event))
-								if (event.type == Event::Closed)
-									Megumin.close();
-
-							Megumin.draw(sprite);
-							Megumin.display();
-						}
-					}
+				if (grid[x][y].isMine == true) {
+					Megumin();					
 				}
 			}
-			// Åñëè êîîðäèíàòû íå ïîïàäàþò íà èãðîâîå ïîëå, à èãðà çàêîí÷åíà, òî ïðîâåðÿåì íàæàòà ëè êíîïêà ðåñåò
-			//else {
-			//	if (game_over && event.key.code == Mouse::Left) {
-			//		if (x >= 4 && x <= 5 && y == field_out.at(0).size()) {
-			//			window.close();
-			//			main();
-			//		}
-			//	}
-			//}
 		}
 	}
 }
 
-void gridPrintText(string text, Font& font, RenderWindow& window, float x_correction, float y_correction) {
-	Text stext(text, font, 24);
-	stext.setPosition(x_correction, y_correction);
-	stext.setFillColor(Color::Black);
+void gridPrintText(std::string text, sf::RenderWindow& window, float xÑorrection, float yÑorrection) {
+	sf::Text stext(text, font, 24);
+	stext.setPosition(xÑorrection, yÑorrection);
+	stext.setFillColor(sf::Color::Black);
 	window.draw(stext);
 }
 
-string gridTime(int seconds) {
-	string result;
+std::string gridTime(int seconds) {
+	std::string result;
 	int iminutes = seconds / 60;
 	int iseconds = seconds % 60;
-	string sminutes = to_string(iminutes);
-	string sseconds = to_string(iseconds);
+	std::string sminutes = std::to_string(iminutes);
+	std::string sseconds = std::to_string(iseconds);
 
 	if (iminutes < 10)
 		sminutes.insert(sminutes.begin(), '0');
@@ -139,62 +190,32 @@ string gridTime(int seconds) {
 	return result;
 }
 
-int main() {
-	srand(time(0));
+void draw(sf::RenderWindow& window) {
+	window.clear(sf::Color::White);
 
-	RenderWindow window(VideoMode(gridWidth * imageWidth, gridHight * imageWidth + 32), "Minesweeper!");
-
-	Texture texture;
-	texture.loadFromFile("images/Tiles.png");
-
-	Font font;
-	font.loadFromFile("CyrilicOld.TTF");
-
-	array <Sprite, 12> sprites;
-	for (size_t i = 0; i < sprites.size(); i++) {
-		sprites.at(i).setTexture(texture);
-		sprites.at(i).setTextureRect(IntRect(i * imageWidth, 0, imageWidth, imageWidth));
+	for (int i = 0; i < gridWidth; i++) {
+		for (int j = 0; j < gridHight; j++) {
+			sf::Sprite sprite = grid[i][j].getSprite();
+			sprite.setPosition(i * imageWidth, j * imageWidth);
+			window.draw(sprite);
+		}
 	}
 
-	Clock clock;
-	clock.restart();
-	int seconds = 0;
+	if (seconds != Clock.getElapsedTime().asSeconds()) {
+		seconds = static_cast<int>(Clock.getElapsedTime().asSeconds());
+		gridPrintText("Time: " + gridTime(seconds), window, 0, gridHight * 32.f);
+	}
+
+	window.display();
+}
+
+int main() {
+	setup();
+
+	sf::RenderWindow window(sf::VideoMode(gridWidth * imageWidth, gridHight * imageWidth + 32), "Minesweeper!");
 
 	while (window.isOpen()) {
-		window.clear(Color::White);
-
-		for (int i = 0; i < gridWidth; i++) {
-			for (int j = 0; j < gridHight; j++) {
-				if (grid[i][j].flag) {
-					sprites[11].setPosition(i * imageWidth, j * imageWidth);
-					window.draw(sprites[11]);
-					continue;
-				}
-
-				if (!grid[i][j].open) {
-					sprites[10].setPosition(i * imageWidth, j * imageWidth);
-					window.draw(sprites[10]);
-					continue;
-				}
-
-				if (grid[i][j].mine) {
-					sprites[9].setPosition(i * imageWidth, j * imageWidth);
-					window.draw(sprites[9]);
-					continue;
-				}
-
-				sprites[grid[i][j].count].setPosition(i * imageWidth, j * imageWidth);
-				window.draw(sprites[grid[i][j].count]);
-			}
-		}
-
-		if (seconds != clock.getElapsedTime().asSeconds()) {
-			seconds = static_cast<int>(clock.getElapsedTime().asSeconds());
-			gridPrintText("Time: " + gridTime(seconds), font, window, 0, gridHight * 32.f);
-		}
-
+		draw(window);
 		gridEvent(window);
-
-		window.display();
 	}
 }
